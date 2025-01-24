e84093d98705cc4204b8f8b67057b08316dafe1c
Satbeer Singh <satbeer@fastor.ai>
2025-01-24 20:58:31 +0530

    init

---
+++ apps/nextjs/app/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+
+import { Button } from "@repo/ui/button";
+import { useState } from "react";
+
+export default function Home() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <main className="container mx-auto p-4">
+      <h1 className="text-xl font-bold mb-2">This is a Next.js application</h1>
+      <p className="mb-4">
+        This shadcn/ui button is shared between Next.js, Vite and any other
+        application.
+      </p>
+      <Button onClick={() => setCount((count) => count + 1)}>
+        Count is {count}
+      </Button>
+    </main>
+  );
+}
---
+++ apps/vite/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+
+import "@repo/ui/globals.css";
+import { Button } from "@repo/ui/button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-xl font-bold mb-2">This is a Vite application</h1>
+      <p className="mb-4">
+        This shadcn/ui button is shared between Vite, NextJS and any other
+        application.
+      </p>
+      <Button onClick={() => setCount((count) => count + 1)}>
+        Count is {count}
+      </Button>
+    </div>
+  );
+}
+
+export default App;
