a4cecdde6f6e7883c38bd8b5068a1155ccf8843e
Satbeer Singh <satbeer@fastor.ai>
2025-01-25 12:19:39 +0530

    font added

--- apps/nextjs/app/layout.tsx
+++ apps/nextjs/app/layout.tsx
@@ -1,12 +1,18 @@
 import { ThemeProvider } from "../components/theme-provider";
 import "@repo/ui/globals.css";
+import { Geist, Poppins } from "next/font/google";
+
+const fontSans = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 export default function RootLayout({ children }: any) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
         <head />
-        <body>
+        <body className={`${fontSans.className} antialiased `}>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
